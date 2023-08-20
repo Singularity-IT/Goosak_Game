14869f3441b8673f76107ed8fa4593db73234c34
Nick Ivashchenko <113722564+Singularity-IT@users.noreply.github.com>
2023-08-20 19:57:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,148 @@
+import pygame
+from pygame.constants import QUIT, K_DOWN , K_UP, K_LEFT, K_RIGHT
+import random
+import os
+
+
+pygame.init()
+
+HEIGHT = 800
+WIDTH = 1200
+
+FONT =pygame.font.SysFont("Verdana",20)
+
+FPS = pygame.time.Clock()
+# Colors
+BLACK_COLOR = (0 ,0 , 0)
+WHITE_COLOR = (255,255,255)
+COLOR_BLUE =(0,0,255)
+COLOR_GREEN =(0,255,0)
+
+bg = pygame.transform.scale(pygame.image.load('background.png'),(WIDTH,HEIGHT))
+bg_X1 = 0
+bg_X2 = bg.get_width()
+bg_move = 3
+
+
+# Enemy Settings
+def create_enemy():
+
+    enemy =  pygame.image.load('enemy.png').convert_alpha()
+    enemy_rect = pygame.Rect(WIDTH,random.randint(enemy.get_height(),HEIGHT -  enemy.get_height()), *enemy.get_size())
+    enemy_move = [random.randint(-8,-4),0]
+    return [enemy, enemy_rect, enemy_move]
+
+CREATE_ENEMY = pygame.USEREVENT +1
+pygame.time.set_timer(CREATE_ENEMY,1500)
+
+enemies = []
+
+# Bonus Settings
+def create_bonus():
+    bonus=pygame.image.load('bonus.png').convert_alpha()
+    bonus_rect = pygame.Rect(random.randint(bonus.get_width(),WIDTH - bonus.get_width()),-bonus.get_height(),*bonus.get_size())
+    bonus_move = [0,random.randint(4,8)]
+    return [bonus, bonus_rect, bonus_move]
+
+CREATE_BONUS = pygame.USEREVENT +2
+pygame.time.set_timer(CREATE_BONUS,2000)
+
+more_bonus = []
+score = 0
+
+main_display = pygame.display.set_mode((WIDTH,HEIGHT))
+
+IMAGE_PATH = "Goose"
+PLAYER_IMAGES = os.listdir(IMAGE_PATH)
+image_index=0
+
+
+player_size =(20,20)
+player=pygame.image.load('player.png').convert_alpha()
+player_rect = player.get_rect()
+player_rect.center = main_display.get_rect().center
+player_move_down = [0,4]
+player_move_right = [4,0]
+player_move_left = [-4,0]
+player_move_top = [0,-4]
+
+CHANGE_IMAGE = pygame.USEREVENT +3
+pygame.time.set_timer(CHANGE_IMAGE,200)
+
+playing = True
+while playing:
+    FPS.tick(120)
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            playing = False
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
+            
+        if event.type == CREATE_BONUS:
+            more_bonus.append(create_bonus())
+            
+        if event.type == CHANGE_IMAGE:
+            player =pygame.image.load(os.path.join(IMAGE_PATH,PLAYER_IMAGES[image_index]))
+            image_index +=1
+            if image_index >=len(PLAYER_IMAGES):
+                image_index =0
+            
+    # main_display.fill(BLACK_COLOR) 
+    bg_X1 -=bg_move
+    bg_X2 -=bg_move
+    
+    if bg_X1 < -bg.get_width():
+        bg_X1 = bg.get_width()
+        
+    if bg_X2 < -bg.get_width():
+        bg_X2 = bg.get_width()
+    
+    main_display.blit(bg,(bg_X1,0))
+    main_display.blit(bg,(bg_X2,0))
+    
+    # Keys r/l/t/d
+    
+    keys = pygame.key.get_pressed()
+    
+    if keys[K_DOWN] and player_rect.bottom<HEIGHT:
+        player_rect = player_rect.move(player_move_down)
+        
+    if keys[K_RIGHT] and player_rect.right<WIDTH:
+        player_rect = player_rect.move(player_move_right)
+        
+    if keys[K_UP] and player_rect.top > 0:
+        player_rect = player_rect.move(player_move_top)  
+        
+    if keys[K_LEFT] and player_rect.left >0:
+        player_rect = player_rect.move(player_move_left)  
+        
+        
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(enemy[2])    
+        main_display.blit(enemy[0], enemy[1])
+        
+        if player_rect.colliderect(enemy[1]):
+            playing = False
+        
+    for bonus in more_bonus:
+        bonus[1] =bonus[1].move(bonus[2])
+        main_display.blit(bonus[0], bonus[1])
+        
+        if player_rect.colliderect(bonus[1]):
+            score+=1
+            more_bonus.pop(more_bonus.index(bonus))
+            
+    main_display.blit(FONT.render(str(score),True,BLACK_COLOR), (WIDTH-50, 20))
+    main_display.blit(player,player_rect)
+    
+    pygame.display.flip()    
+    
+    for enemy in enemies:
+        if enemy[1].right <0:
+            enemies.pop(enemies.index(enemy))
+    for bonus in more_bonus:
+        if bonus[1].bottom >HEIGHT:
+            more_bonus.pop(more_bonus.index(bonus))
+            
+   
